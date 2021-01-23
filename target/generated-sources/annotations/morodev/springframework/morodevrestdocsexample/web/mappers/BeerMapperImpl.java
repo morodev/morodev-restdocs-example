faebcef47d1f39c34ee1afc725dc825134e1cc31
Luca Moro <71976553+morodev@users.noreply.github.com>
2021-01-23 12:10:22 +0100

    97. Documentation Generation

--- target/generated-sources/annotations/morodev/springframework/morodevrestdocsexample/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/morodev/springframework/morodevrestdocsexample/web/mappers/BeerMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-01-23T11:25:49+0100",
+    date = "2021-01-23T11:56:10+0100",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.8 (Amazon.com Inc.)"
 )
 @Component
